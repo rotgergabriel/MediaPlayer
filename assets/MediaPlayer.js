7b36b5876d63205f1738415ed5e175262b236d8c
Gabriel <rotgergabriel@gmail.com>
2020-11-09 01:44:34 -0300

    feat-Multiples modifications

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-
-function MediaPlayer(config) {
-    this.media = config.el;
-    this.plugins = config.plugins || [];
-
-    this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function() {
-    this.plugins.forEach(plugin => {
-    plugin.run(this);
-    });
-};
-
-MediaPlayer.prototype.play = function() {
-    this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-    this.play();
-    } else {
-    this.pause();
-    }
-};
-
-
-=======
 function MediaPlayer(config) {
   this.media = config.el;
   this.plugins = config.plugins || [];
@@ -67,6 +34,5 @@ MediaPlayer.prototype.mute = function() {
 MediaPlayer.prototype.unmute = function() {
   this.media.muted = false;
 };
->>>>>>> b21f591d91a6d57e6308d9de2f713500166e79c2
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -9,19 +9,77 @@ playButton.onclick = () => player.togglePlay();
 
 const muteButton = document.querySelector('#muteButton');
 muteButton.onclick = () => {
-<<<<<<< HEAD
-    if (player.media.muted) {
-        player.unmute();
-        this.media.muted = false
-    } else {
-        player.mute();
-        this.media.muted = true
-    }
-=======
   if (player.media.muted) {
     player.unmute();
   } else {
     player.mute();
   }
->>>>>>> b21f591d91a6d57e6308d9de2f713500166e79c2
 };
+
+// function saludar() {
+//   console.log(`Hola me llamo ${this.name} ${this.surname}`)
+// }
+
+// let persona = {
+//   name:'Gabriel',
+//   surname:'Rotger'
+// }
+
+// saludar.call(persona)
+
+// function correr(metros, direccion) {
+//   console.log(`Corro ${metros} metros y en direccion ${direccion} todos los dias`)
+// }
+// const valores = [400, "sur"]
+
+// correr.apply(persona, valores )
+
+
+// let personaDos = {
+//   name:'Sergio',
+//   surname:'Rotger'
+// }
+// const personaDosSaluda = saludar.bind(personaDos)
+
+// personaDosSaluda()
+
+// const personaDosCorrer = correr.bind(personaDos, 100, 'cualquier direccion')
+// personaDosCorrer()
+
+// const zelda = {
+//   name: 'Zelda',
+// };
+
+// const link = {
+//   name: 'Link',
+// };
+
+// zelda.saludar = function() {
+//   console.log(`Hola soy ${this.name}`)
+// }
+// link.saludar = function() {
+//   console.log(`Hola soy ${this.name}`)
+// }
+
+// zelda.saludar();
+// link.saludar()
+// const heroMethods = {
+//   saludar: function() {
+//     console.log(`Hola soy ${this.name}`)
+//   }
+// }
+
+function Hero(name) {
+  // const hero = Object.create(Hero.prototype);
+    this.name = name;
+}
+
+Hero.prototype.saludar = function() {
+  console.log(`Hola soy ${this.name}`)
+}
+
+  const zelda = new Hero('Zelda')
+  zelda.saludar()
+
+  const link = new Hero('Link')
+  link.saludar()
